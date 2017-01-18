8d14d2aa44360c8fdaa35224e06409096314f492
Axel Vasquez <axelv@squez.com.ar>
2017-01-18 15:22:22 -0300

    First upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+	'use strict';
+
+	fixSizes();
+
+	// Loader
+	$('.loader-content').delay(2000).fadeToggle('slow');
+	$('.loader').delay(2500).fadeOut('slow', function() {
+		$('body').css('overflow', 'auto');
+	});
+
+	// Hash links
+	$('a[href^="#"]').on('click',function(e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 150
+	    }, 400, 'swing', function() {
+	        window.location.hash = target;
+	    });
+	});
+
+	// Works "hover" effect
+	$('.work-item').bind('mouseenter mouseleave', function() {
+		$(this).children('.work-item-content').toggleClass('visually-showed');
+	});
+
+	// Remodal events
+	$(document).on('opening', '.remodal', function() {});
+	$(document).on('opened', '.remodal', function() {});
+	$(document).on('closing', '.remodal', function() {});
+	$(document).on('closed', '.remodal', function() {});
+
+	//Get in touch effects
+	$('#contact a').on('mouseenter mouseleave', function() {
+		if ($(this).hasClass('email')) {
+			$('#contact').find('h1').toggleClass('email-color');
+		} else if ($(this).hasClass('twitter')) {
+			$('#contact').find('h1').toggleClass('twitter-color');
+		} else if ($(this).hasClass('instagram')) {
+			$('#contact').find('h1').toggleClass('instagram-color');
+		} else if ($(this).hasClass('dribbble')) {
+			$('#contact').find('h1').toggleClass('dribbble-color');
+		} else if ($(this).hasClass('github')) {
+			$('#contact').find('h1').toggleClass('github-color');
+		} else if ($(this).hasClass('linkedin')) {
+			$('#contact').find('h1').toggleClass('linkedin-color');	
+		};
+	});
+
+	// Scroll reveal
+	var config = { 
+		mobile: false
+	}
+
+	window.sr = new scrollReveal(config);
+
+});
+
+$(window).resize(function() {
+	'use strict'
+
+	fixSizes();
+});
+
+function fixSizes() {
+	// Home position
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	$('#home').css('height', windowHeight);
+	$('#home h1').each(function() {
+		$(this).css('padding-top', ($(this).parent().height() - $(this).height()) / 2);
+	});
+};
+
+
+
+
+
+
+
+
