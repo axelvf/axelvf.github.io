49d645daf72685a910e92c25dc71955a0794c5ea
iamcgd <hola@iamcgd.com>
2017-01-21 14:46:05 -0300

    Header
    No me gustaba q tu nombre sea del mismo tamaño q los botones
    jajajaja

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top - 150
+	        'scrollTop': $target.offset().top - 0
 	    }, 400, 'swing', function() {
 	        window.location.hash = target;
 	    });
