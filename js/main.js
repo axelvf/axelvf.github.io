42ae96a3baa63436fb6da38d4e3bb1fb2fbe5f90
Axel Vasquez <axelv@squez.com.ar>
2021-04-10 15:00:31 -0300

    Formatter

--- js/main.js
+++ js/main.js
@@ -10,47 +10,47 @@ $(document).ready(function() {
     });
 
     // write message in c0nsol3 :)
-	console.log("mysite@axelvf:~$");
-	console.log("mysite@axelvf:~$");
-	console.log("mysite@axelvf:~$");
-	console.log("mysite@axelvf:~$ What are you doing here?");
-	console.log("mysite@axelvf:~$");
+    console.log("mysite@axelvf:~$");
+    console.log("mysite@axelvf:~$");
+    console.log("mysite@axelvf:~$");
+    console.log("mysite@axelvf:~$ What are you doing here?");
+    console.log("mysite@axelvf:~$");
 
     // Hash links
- $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 700, function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 700, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
             }
-        }
-    });
+        });
 
     // safe reference. We dont want this the be done every scrolled pixel!
     // (Courtesy: Martijn :  http://stackoverflow.com/users/2519416/martijn)
@@ -103,16 +103,16 @@ function fixSizes() {
     });
 };
 
- // Change header nav on scroll
+// Change header nav on scroll
 
-$(function(){
-$(window).on('scroll', function() {
-    var scrollTop = $(this).scrollTop();
-    $('section').each(function() {
-        var topDistance = $(this).offset().top - $('header').outerHeight();
-        if ( (topDistance) < scrollTop ) {
-            $('.menu a').css('color',$(this).attr('data-color'));
-        }
+$(function() {
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+        $('section').each(function() {
+            var topDistance = $(this).offset().top - $('header').outerHeight();
+            if ((topDistance) < scrollTop) {
+                $('.menu a').css('color', $(this).attr('data-color'));
+            }
+        });
     });
-});
 })
